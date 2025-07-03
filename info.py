8ccf67502f1133dad388a83fea9bc6d5fbd4784a
tmayur <thakkarmayur071@gmail.com>
2025-07-03 21:26:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,16 +13,16 @@ def is_enabled(value, default):
         return default
 
 #main variables
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6165486744:AAFW3C6588558QbLWfRD_-ddGAWAfY')
+API_ID = int(environ.get('API_ID', '28594907'))
+API_HASH = environ.get('API_HASH', 'bc291485282a77e015127cf425115b38')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7843397618:AAH81DdY2N6V1YdJaGkwjyQyjK6ttRyBUHs')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5397984467').split()]
-USERNAME = environ.get('USERNAME', "https://telegram.me/biisal")
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-10019494867581'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+4RB2-U2o9yE4ZmQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001234567898').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://username:password@cluster0.jhvfd.mongodb.net/?retryWrites=true&w=majority")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '521878164').split()]
+USERNAME = environ.get('USERNAME', "https://telegram.me/M1612T")
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001622092675'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviesgroupforyou')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001678911732').split()]
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://MovieDB:MovieDB@moviedb.kxuobnb.mongodb.net/?retryWrites=true&w=majority&appName=MovieDB")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
